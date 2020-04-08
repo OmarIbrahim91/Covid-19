c29d000222a18d39dc493da42d52c57a185f6a87
root <omarali43620@yahoo.com>
2020-04-08 20:57:21 +0200

    Updates

---
+++ precache-manifest.69e80d55cea5b81a87b53695ca9d884c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3eacbe8dec08e61de7db8b5fe37c7bf",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "fa5d57b3f0bc833a267f",
+    "url": "/Covid-19/static/css/main.29cf1277.chunk.css"
+  },
+  {
+    "revision": "e54a42207202103b2377",
+    "url": "/Covid-19/static/js/2.901ebb04.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.901ebb04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5d57b3f0bc833a267f",
+    "url": "/Covid-19/static/js/main.c947664c.chunk.js"
+  },
+  {
+    "revision": "0a4c34d309d14e06d152",
+    "url": "/Covid-19/static/js/runtime-main.38136993.js"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/Covid-19/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "48a2133dd5c34ad46cc6ab6ff06c03ec",
+    "url": "/Covid-19/static/media/linkedin.48a2133d.svg"
+  },
+  {
+    "revision": "1aae2673bc61817b9c30745c3687c0ec",
+    "url": "/Covid-19/static/media/xing.1aae2673.svg"
+  }
+]);
